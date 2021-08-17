ce28f270c3045d5a3271e9c031696d243169121e
Amorn Apichattanakul <>
2021-08-17 15:38:59 +0700

    first committ

---
+++ android/app/src/main/kotlin/com/example/ise/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ise
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/dog.dart
@@ -0,0 +1,11 @@
+class Dog {
+  String? name;
+  int? age;
+
+  Dog(this.name, this.age);
+
+  void bark() {
+    print("woof woof $age");
+  }
+    
+}
